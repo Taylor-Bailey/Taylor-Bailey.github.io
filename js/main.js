f719703387d8eeedd0d4c4bee04a8e5301f946e2
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-01-29 23:17:43 -0600

    upsated nav bar

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// function hover(element) {
+//     element.setAttribute('src', '../images/twb-03.png');
+// }
+// function unhover(element) {
+//     element.setAttribute('src', '../images/twb-0.png');
+// }
