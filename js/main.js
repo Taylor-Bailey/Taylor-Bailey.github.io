a5f776a0450a93a0e3dd6ded97dc8c1d1f3fe3b2
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-02-03 16:32:09 -0600

    updated nav

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@
 // function unhover(element) {
 //     element.setAttribute('src', '../images/twb-0.png');
 // }
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
