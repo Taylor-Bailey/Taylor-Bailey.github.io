bc974b100520b8c645f595ee88e9c0629c7fa929
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-04-20 15:52:40 -0500

    redesign-nav and logo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// function hover(element) {
-//     element.setAttribute('src', '../images/twb-03.png');
-// }
-// function unhover(element) {
-//     element.setAttribute('src', '../images/twb-0.png');
-// }
+'use strict';
+let $ = require("jquery");
 
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
